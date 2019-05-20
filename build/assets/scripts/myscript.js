1cb0dc926fd010824e7f50b52695e41dfd31aee2
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-05-20 14:56:02 +0300

    animation fix

--- build/assets/scripts/myscript.js
+++ build/assets/scripts/myscript.js
@@ -7,10 +7,10 @@ $(document).ready(function () {
       } else {
         $(".erp-container-tabs-item").each(function(){
           $(this).removeClass('active');
-          $(document).find('.erp-container-content-text').eq($(this).index()).css('display', 'none');
+          $(document).find('.erp-container-content-text-item').eq($(this).index()).css('display', 'none');
         });
         $(this).addClass('active');        
-        $(document).find('.erp-container-content-text').eq(tab_index).fadeIn('slow');
+        $(document).find('.erp-container-content-text-item').eq(tab_index).fadeIn(1800);
         console.log(tab_index);
         if(tab_index == 0) {
           $(document).find('.erp-container-content-graphics-erp').addClass('slide1').removeClass('slide2').removeClass('slide3').removeClass('slide4');
@@ -25,13 +25,13 @@ $(document).ready(function () {
           $(document).find('.erp-container-content-graphics-business').addClass('slide2').removeClass('slide1').removeClass('slide3').removeClass('slide4');       
         }
         if(tab_index == 2) {
-          $(document).find('.erp-container-content-graphics-erp').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4');
+          $(document).find('.erp-container-content-graphics-erp').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4').css('display', 'block');;
           $(document).find('.erp-container-content-graphics-star').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4');
           $(document).find('.erp-container-content-graphics-ddm').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4'); 
           $(document).find('.erp-container-content-graphics-business').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4');       
         }
         if(tab_index == 3) {
-          $(document).find('.erp-container-content-graphics-erp').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3');
+          $(document).find('.erp-container-content-graphics-erp').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3').css('display', 'block');;
           $(document).find('.erp-container-content-graphics-star').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3');
           $(document).find('.erp-container-content-graphics-ddm').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3'); 
           $(document).find('.erp-container-content-graphics-business').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3');       
