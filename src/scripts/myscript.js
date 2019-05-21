aaea67baf804462e5576b26e3d13bb86fe1b071a
Adnrey Lavrov <fanton96mailru@gmail.com>
2019-05-21 12:33:11 +0300

    erp block fix

--- src/scripts/myscript.js
+++ src/scripts/myscript.js
@@ -25,17 +25,47 @@ $(document).ready(function () {
           $(document).find('.erp-container-content-graphics-business').addClass('slide2').removeClass('slide1').removeClass('slide3').removeClass('slide4');       
         }
         if(tab_index == 2) {
-          $(document).find('.erp-container-content-graphics-erp').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4').css('display', 'block');;
+          $(document).find('.erp-container-content-graphics-erp').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4');
           $(document).find('.erp-container-content-graphics-star').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4');
           $(document).find('.erp-container-content-graphics-ddm').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4'); 
           $(document).find('.erp-container-content-graphics-business').addClass('slide3').removeClass('slide1').removeClass('slide2').removeClass('slide4');       
         }
         if(tab_index == 3) {
-          $(document).find('.erp-container-content-graphics-erp').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3').css('display', 'block');;
+          $(document).find('.erp-container-content-graphics-erp').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3');
           $(document).find('.erp-container-content-graphics-star').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3');
           $(document).find('.erp-container-content-graphics-ddm').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3'); 
           $(document).find('.erp-container-content-graphics-business').addClass('slide4').removeClass('slide1').removeClass('slide2').removeClass('slide3');       
         }
       }
     });
+});
+
+$(document).ready(function() {
+  $('.minicase-block-slider').slick({
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    draggable: true,
+    focusOnSelect: true,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          vertical: false,
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }        
+      },
+      {
+        breakpoint: 425,
+        settings: {
+          vertical: false,
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }        
+      }      
+    ]
+  });
 });
